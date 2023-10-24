4cc1329f7e990d474132b0afe658ae3e215f3739
Rafael Delboni <rafadelboni@gmail.com>
2023-10-24 15:12:52 -0300

    refact: using animation tree

---
+++ scenes/player/animation.gd
@@ -0,0 +1,29 @@
+extends AnimationTree
+@onready var animation_player = $"../AnimationPlayer"
+var moving_blend_path := "parameters/Move/blend_position"
+
+# False : set animation to "idle"
+# True : set animation to "move"
+@onready var moving : bool = false : set = set_moving
+
+# Blend value between the walk and run cycle
+# 0.0 walk - 1.0 run
+@onready var move_speed : float = 0.0 : set = set_moving_speed
+@onready var state_machine : AnimationNodeStateMachinePlayback = self.get("parameters/playback")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.active = true
+	animation_player["playback_default_blend_time"] = 0.2
+
+func set_moving(value : bool):
+	moving = value
+	if moving:
+		state_machine.travel("Move")
+	else:
+		state_machine.travel("Idle")
+
+func set_moving_speed(value : float):
+	print(move_speed)
+	move_speed = clamp(value, 0.0, 1.0)
+	self.set(moving_blend_path, move_speed)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,8 @@
 extends CharacterBody3D
 
-@onready var animation_player : AnimationPlayer = $AnimationPlayer
-@onready var visuals : Node3D = $visuals
-@onready var last_strong_direction : Vector3 = Vector3.FORWARD
-var walking : bool = false
-var running : bool = false
-
 ## Character maximum speed on the ground.
 @export var walk_speed : float = 3.0
-@export var run_speed : float = 6.0
+@export var run_speed : float = 5.0
 ## Movement acceleration (how fast character achieve maximum speed)
 @export var acceleration := 5.0
 ## Player model rotaion speed
@@ -16,30 +10,28 @@ var running : bool = false
 ## Stopping speed between the idle and running states.
 @export var stopping_speed := 1.0
 
+@onready var speed : float = walk_speed
+@onready var visuals : Node3D = $visuals
+@onready var last_strong_direction : Vector3 = Vector3.FORWARD
+@onready var animation_tree = $AnimationTree
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func start_walking() -> void :
-	walking = true
-	if not running:
-		animation_player.play("Walking")
-	else:
-		animation_player.play("Running")
+func get_input() -> Vector3:
+	var raw_input := Input.get_vector("left", "right", "forward", "backward")
+	
+	var input = (transform.basis * Vector3(raw_input.x, 0, raw_input.y)).normalized()
+	
+#	var input := Vector3.ZERO
+#
+#	# This is to ensure that diagonal input isn't stronger than axis aligned input
+#	input.x = raw_input.x * sqrt(1.0 - raw_input.y * raw_input.y / 2.0)
+#	input.z = raw_input.y * sqrt(1.0 - raw_input.x * raw_input.x / 2.0)
+#	input.y = 0.0
 
-func stop_walking() -> void :
-	walking = false
-	animation_player.play("Idle")
-	
-func start_running() -> void :
-	running = true
-	if walking:
-		animation_player.play("Running")
-	
-func stop_running() -> void :
-	running = false
-	if walking:
-		animation_player.play("Walking")
-		
+	return input
+
 func orient_character_to_direction(direction: Vector3, delta: float) -> void:
 	var left_axis := Vector3.UP.cross(direction)
 	var rotation_basis := Basis(left_axis, Vector3.UP, direction).get_rotation_quaternion()
@@ -47,14 +39,6 @@ func orient_character_to_direction(direction: Vector3, delta: float) -> void:
 	visuals.transform.basis = Basis(visuals.transform.basis.get_rotation_quaternion().slerp(rotation_basis, delta * rotation_speed)).scaled(
 		model_scale
 	)
-	
-func _ready() -> void:
-	animation_player.set_blend_time("Idle", "Walking", 0.2)
-	animation_player.set_blend_time("Walking", "Idle", 0.2)
-	animation_player.set_blend_time("Walking", "Running", 0.25)
-	animation_player.set_blend_time("Running", "Walking", 0.25 )
-	animation_player.set_blend_time("Running", "Idle", 0.2)
-	animation_player.set_blend_time("Idle", "Running", 0.2)
 
 func _physics_process(delta) -> void:
 	# Add the gravity.
@@ -62,8 +46,7 @@ func _physics_process(delta) -> void:
 		velocity.y -= gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = get_input()
 	
 	# To not orient quickly to the last input, we save a last strong direction,
 	# this also ensures a good normalized value for the rotation basis.
@@ -74,22 +57,18 @@ func _physics_process(delta) -> void:
 	
 	# Check inputs to set Run
 	if Input.is_action_just_pressed("run"):
-		start_running()
+		speed = run_speed
 	if Input.is_action_just_released("run"):
-		stop_running()
+		speed = walk_speed
 	
-	# Calculate speed based if player is running or walking
-	var speed : float = walk_speed if not running else run_speed
-
 	# Check velocity to set walking state
 	if direction.length() == 0 and velocity.length() < stopping_speed:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-		if walking:
-			stop_walking()
+		animation_tree.set_moving(false)
 	else:
 		velocity = velocity.lerp(direction * speed, acceleration * delta)
-		if not walking:
-			start_walking()
+		animation_tree.set_moving(true)
+		animation_tree.set_moving_speed(inverse_lerp(walk_speed, run_speed, velocity.length()))
 
 	move_and_slide()
